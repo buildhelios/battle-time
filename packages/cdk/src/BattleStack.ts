3e7f391e9e7b7e9077b921131fd6d00e4dc41f09
Scott Vance <scott@iyio.io>
2022-10-23 11:10:54 -0400

    - deploy each branch to different s3 bucket

--- packages/cdk/src/BattleStack.ts
+++ packages/cdk/src/BattleStack.ts
@@ -1,7 +1,4 @@
 import * as cdk from 'aws-cdk-lib';
-import * as cm from "aws-cdk-lib/aws-certificatemanager";
-import * as cf from "aws-cdk-lib/aws-cloudfront";
-import * as cfo from "aws-cdk-lib/aws-cloudfront-origins";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from "aws-cdk-lib/aws-s3";
@@ -12,11 +9,18 @@ export class BattleStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        let branch:string = this.node.tryGetContext('git_branch');
+
+        if(!branch){
+            throw new Error('git_branch context variable required')
+        }
+        branch=branch.replace(/\W/g,'_');
+
         this.createFunction('api',{
             createPublicUrl:true
         })
 
-        this.createWebSite('battle-app',null);
+        this.createWebSite('battle-app',branch);
     }
 
     createFunction(name: string, {
@@ -51,51 +55,23 @@ export class BattleStack extends cdk.Stack {
         return func;
     }
 
-    createWebSite(name: string, domainName:string|null){
+    createWebSite(name: string, branch:string){
 
-        let certificate:cm.Certificate|undefined=undefined;
-
-        if(domainName){
-            certificate = new cm.Certificate(this, "CustomDomainCertificate", {
-                domainName: domainName,
-                validation: cm.CertificateValidation.fromDns()
-            });
-            new cdk.CfnOutput(this,`websiteDomain00${name}`,{value:domainName});
-        }
-
-        const bucket=new s3.Bucket(this,'website-'+name,{
-            accessControl:s3.BucketAccessControl.PRIVATE,
-            autoDeleteObjects:true,
-            removalPolicy:cdk.RemovalPolicy.DESTROY
+        const bucket=new s3.Bucket(this,'website-'+name+'-'+branch,{
+            publicReadAccess:true,
+            removalPolicy:cdk.RemovalPolicy.RETAIN,
+            websiteIndexDocument:"index.html",
+            websiteErrorDocument:"index.html",
         });
 
-        const distribution = new cf.Distribution(this, 'Distribution', {
-            defaultBehavior: { origin: new cfo.S3Origin(bucket) },
-        });
-
-        new s3Deployment.BucketDeployment(this,`website-${name}-deployment`,{
+        new s3Deployment.BucketDeployment(this,`website-${name}-deployment-${branch}`,{
             destinationBucket:bucket,
-            distributionPaths:['/*'],
-            distribution,
             sources:[
                 s3Deployment.Source.asset(`../../dist/packages/${name}/exported`)
             ]
         });
 
-        const originAccessIdentity=new cf.OriginAccessIdentity(this, `website-${name}-origin-ident`);
-        bucket.grantRead(originAccessIdentity);
-
-        const dist=new cf.Distribution(this, `website-${name}-distribution`, {
-            defaultRootObject: 'index.html',
-            defaultBehavior: {
-                origin:new cfo.S3Origin(bucket,{originAccessIdentity}),
-                viewerProtocolPolicy: cf.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-            },
-            certificate,
-            domainNames:domainName?[domainName]:undefined
-        });
-
-        new cdk.CfnOutput(this,`websiteUrl00${name}`,{value:`https://${dist.domainName}`});
+        new cdk.CfnOutput(this,`websiteUrl00${name}`,{value:`https://${bucket.bucketDomainName}/index.html`});
 
     }
 }
